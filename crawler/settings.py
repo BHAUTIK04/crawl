2cd0b89803fc6fcea4201ce351d464a8f007e1b8
BHAUTIK04 <sens.bhautik@gmail.com>
2018-05-06 16:47:20 +0530

    Allowed host

--- crawler/settings.py
+++ crawler/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's4pgi0bcg8tcbn05v10i14-3pn(=$zz^!xmqf-$*(7hz&%x1a9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
